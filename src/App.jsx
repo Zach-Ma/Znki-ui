91ed0d9b996cc091c95fa425498acd8f55320555
Zchary-Ma <Zcharyma@gmail.com>
2021-04-17 23:09:53 +0800

    chore: update page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink,
+  Link,
+} from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
-    <div>
-      <Homepage count={12} />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,11 +2,99 @@ import React from "react";
 import PropTypes from "prop-types";
 //import { Test } from './Homepage.styles';
 import Card from "../../components/Card";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  MenuItem,
+  Menu,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AddIcon from "@material-ui/icons/Add";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import AccountCircle from "@material-ui/icons/AccountCircle";
 
 const CardList = ({ count = 1, ...props }) =>
   Array.from({ length: count }, (item, index) => <Card key={index} />);
 
-const Homepage = () => <CardList count={10} />;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Homepage = () => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <React.Fragment>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Photos
+          </Typography>
+
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
 
 Homepage.propTypes = {
   // bla: PropTypes.string,
