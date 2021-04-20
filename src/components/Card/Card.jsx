487b78803449817f3b97b85f9e9c0813315bdca8
Zchary-Ma <Zcharyma@gmail.com>
2021-04-20 20:10:20 +0800

    add(DeckCard):add deck card styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,57 +1,102 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import { Chip } from "@material-ui/core";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-
+import PropTypes from "prop-types";
+import PublicIcon from "@material-ui/icons/Public";
+import AssignmentTurnedInIcon from "@material-ui/icons/AssignmentTurnedIn";
 const useStyles = makeStyles({
   root: {
-    minWidth: 275,
+    paddingLeft: ".75rem",
+    minWidth: 200,
+    maxWidth: "90%",
+    boxShadow: "9px 6px 13px 0px  rgba(34, 35, 58, 0.2)",
+    borderRadius: 12,
   },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
+  overline: {
+    fontSize: ".75rem",
   },
   title: {
-    fontSize: 14,
+    padding: "0.5rem 0 0.3rem",
   },
-  pos: {
-    marginBottom: 12,
+  description: {},
+  actions: {
+    paddingBottom: "1rem",
+    justifyContent: "space-between",
+    "& button": {
+      flexShrink: 0,
+    },
   },
 });
 
-export default function DeckCard() {
+export default function DeckCard({
+  version = 0,
+  pid = -1,
+  conf = {},
+  createAt = "2021-04-17T09:07:12.180Z",
+  updateAt = "2021-04-17T09:07:12.180Z",
+  isPublic = false,
+  name = "placeholder",
+  description = "Bedfordshire tangible Refined",
+}) {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
-
   return (
     <Card className={classes.root}>
       <CardContent>
         <Typography
-          className={classes.title}
+          className={classes.overline}
           color="textSecondary"
           gutterBottom
         >
-          Word of the Day
+          {createAt}
         </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
+        <Typography
+          color="primary"
+          className={classes.title}
+          variant="h5"
+          component="h2"
+        >
+          {name}
         </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
+        <Typography color="textSecondary" className={classes.description}>
+          {description}
         </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Typography variant="body2" component="p"></Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
+      <CardActions className={classes.actions}>
+        <Chip
+          variant="outlined"
+          color="primary"
+          size="small"
+          label={isPublic ? "public" : "private"}
+          icon={<PublicIcon />}
+        />
+        <Chip
+          variant="outlined"
+          color="primary"
+          size="small"
+          label="placeholder"
+          icon={<AssignmentTurnedInIcon />}
+        />
+        <Button color="primary" size="small">
+          view
+        </Button>
       </CardActions>
     </Card>
   );
 }
+
+DeckCard.propTypes = {
+  version: PropTypes.number,
+  pid: PropTypes.number,
+  conf: PropTypes.object,
+  createAt: PropTypes.string,
+  updateAt: PropTypes.string,
+  isPublic: PropTypes.bool,
+  name: PropTypes.string,
+  description: PropTypes.string,
+};
